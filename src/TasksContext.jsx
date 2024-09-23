d8f290bd3278aa1de9a05e4b9d8a18870d144ef4
Eze <eze.gonzalez@vera.com.uy>
2024-09-23 19:37:18 -0300

    Prontas las requests para el back

--- src/TasksContext.jsx
+++ src/TasksContext.jsx
@@ -29,23 +29,65 @@ export function TasksManager({ children }) {
             const id = task.id;
             const url = `${this.url}/${id}`;
 
-            if (this.tasks.includes(task)) {
-                const response = await fetch(url, {
-                    method: "DELETE",
-                    params: id,
-                    headers: {
-                        "Content-Type": "text/xml",
-                    },
-                });
+            const response = await fetch(url, {
+                method: "DELETE",
+                params: id,
+                headers: {
+                    "Content-Type": "text/xml",
+                },
+            });
 
-                const status = `dashboard.deleteTask(): ${response.status}, ${response.statusText}`;
-                console.log(status);
+            const status = `dashboard.deleteTask(): ${response.status}, ${response.statusText}`;
+            console.log(status);
 
-                if (!response.ok)
-                    return;
+            if (!response.ok)
+                return;
 
-                this.setTasks(this.tasks.filter(keep => keep !== task)); // sacamos del array
-            }
+            this.setTasks(this.tasks.filter(keep => keep !== task)); // sacamos del array
+        },
+
+        async addTask(task) {
+            const response = await fetch(this.url, {
+                method: "POST",
+                body: JSON.stringify(task),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+
+            const status = `dashboard.addTask(): ${response.status}, ${response.statusText}`;
+            console.log(status);
+
+            if (!response.ok)
+                return;
+
+            // Descarto lo que acabo de crear y uso lo que me devuelve el sv
+            const res_json = await response.json();
+            this.setTasks(tasks.concat([res_json]));
+        },
+
+        async editTask(task) {
+            const id = task.id;
+            const url = `${this.url}/${id}`;
+
+            console.log(id)
+            const response = await fetch(url, {
+                method: "PUT",
+                params: id,
+                body: JSON.stringify(task),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+
+            const status = `dashboard.editTask(): ${response.status}, ${response.statusText}`;
+            console.log(status);
+
+            if (!response.ok)
+                return;
+
+            const res_json = await response.json();
+            this.setTasks(this.tasks.filter(keep => keep.id !== task.id).concat([res_json]));
         },
     }
 
