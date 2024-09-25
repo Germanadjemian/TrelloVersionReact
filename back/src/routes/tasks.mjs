a128cd690b9235ba120bf427316386536289c3f3
Eze <eze.gonzalez@vera.com.uy>
2024-09-25 09:02:25 -0300

    juntamos back y front

---
+++ back/src/routes/tasks.mjs
@@ -0,0 +1,79 @@
+import express from "express";
+const router = express.Router();
+
+let lastId = 3;
+
+// Sample data (tasks)
+let tasks = [
+  {
+    id: "1",
+    title: "Task 1",
+    description: "Description for Task 1",
+    assignedTo: "Rodrigo Lujambio",
+    startDate: "2024-01-01",
+    endDate: "2024-12-31",
+    status: "To Do",
+    priority: "Low",
+    comments: [],
+  },
+  {
+    id: "2",
+    title: "Task 2",
+    description: "Description for Task 2",
+    assignedTo: "Michel Sampil",
+    startDate: "2024-01-01",
+    endDate: "2024-12-31",
+    status: "In Progress",
+    priority: "Medium",
+    comments: [],
+  },
+  {
+    id: "3",
+    title: "Task 3",
+    description: "Description for Task 3",
+    assignedTo: "Jose Abadie",
+    startDate: "2024-01-01",
+    endDate: "2024-12-31",
+    status: "Done",
+    priority: "High",
+    comments: [],
+  },
+];
+
+// GET all tasks
+router.get("/tasks", (req, res) => {
+  res.json(tasks);
+});
+
+// POST a new task
+router.post("/tasks", (req, res) => {
+  const task = req.body;
+  lastId++;
+  task.id = (lastId).toString();
+  tasks.push(task);
+  res.status(201).json(task);
+});
+
+// DELETE a task by ID
+router.delete("/tasks/:taskId", (req, res) => {
+  const taskId = req.params.taskId;
+  tasks = tasks.filter((task) => task.id !== taskId);
+  res.sendStatus(204);
+});
+
+// PUT (update) a task by ID
+router.put("/tasks/:taskId", (req, res) => {
+  const taskId = req.params.taskId;
+  const updatedTask = req.body;
+
+  tasks = tasks.map((task) => {
+    if (task.id === taskId) {
+      return { ...task, ...updatedTask, id: taskId };
+    }
+    return task;
+  });
+
+  res.json(tasks.find((task) => task.id === taskId));
+});
+
+export default router;
